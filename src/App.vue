f929f4ac1209fd15a473029947ddf99e7a6eb667
Gianluca-Basili <gianluc.basili@gmail.com>
2023-10-06 19:57:54 +0200

    creazione header

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components:{
+    AppHeader
+  }
+}
+</script>
+<template lang="">
+  <div>
+    <AppHeader />
+  </div>
+</template>
+<style lang="scss" scoped>
+  @import '../styles/generals.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  
+}
+</script>
+<template>
+  <header>
+    <div class="container navbar d-flex justify-content-center">
+      <div class="row">
+        <div class="col-12">
+          <img src="../logo/logo.jpg" alt="" height="80">
+        </div>
+      </div>
+      <ul class="navbar-nav ">
+        <!-- home  -->                        
+        <li class="nav-item nav_item_hover mx-5">
+            <router-link :to="'/'" class="nav-link text-dark ms-0 ms-md-2 fs-6 p-1 ">
+                Home
+            </router-link>
+        </li>
+        
+    </ul>
+    <ul class="navbar-nav  ">
+        <!-- contatti  -->                        
+        <li class="nav-item nav_item_hover mx-5">
+            <router-link :to="'/'" class="nav-link text-dark ms-0 ms-md-2 fs-6 p-1 ">
+                contatti
+            </router-link>
+        </li>
+    </ul>
+    <ul class="navbar-nav ">
+        <!-- chi siamo -->                        
+        <li class="nav-item nav_item_hover mx-5">
+            <router-link :to="'/'" class="nav-link text-dark ms-0 ms-md-2 fs-6 p-1 ">
+              chi siamo
+            </router-link>
+        </li>
+    </ul>
+    </div>
+    
+  </header>
+</template>
+<style lang="scss" scoped>
+  @import '../styles/generals.scss';
+  .nav_item_hover:hover {
+    font-weight: 900;
+    border-bottom: 1px solid;
+    border-width: thin;
+}
+
+</style>
