e3f1d9131119b3df32b84bedcbd453398e76696f
Gianluca-Basili <gianluc.basili@gmail.com>
2023-10-13 13:50:45 +0200

    aggiunta immagine contatti

--- src/components/Contatti.vue
+++ src/components/Contatti.vue
@@ -4,10 +4,31 @@ export default {
 }
 </script>
 <template lang="">
+    <div class="container-fluid immaginecontatti">
+        <div class="row">
+            <div class=" container col-sm-12 col-md-12 text-center  ">
+                <img src="../assets/img/immaginecontatti.jpeg" alt="10"  class="rounded web" height=700>
+
+            </div>
+        </div>
+    </div>
     <div>
        <h1 class="text-center my-5">Il sito è ancora in fase di manutenzione</h1>
     </div>
+
 </template>
 <style lang="scss" scoped>
-    
+    .immaginecontatti{
+        background-color: lightcyan;
+    }
+
+    @media screen and (max-width: 767px) {
+        .immaginecontatti{
+        background-color: white;
+    }
+    .web {
+        width: -webkit-fill-available;
+        
+    }
+}
 </style>
