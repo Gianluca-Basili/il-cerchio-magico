c50816c887a65636e80b5e73f511366dc23f8541
Gianluca-Basili <gianluc.basili@gmail.com>
2023-10-13 11:23:22 +0200

    aggiune icone social e correzione cell

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,22 +8,32 @@ export default {
     <div>
       <div class="container-fluid bg-dark text-white mt-5">
             <div class="container">
-                <h1 class="text-center">Contatti</h1>
+                <h1 class="text-center my-3">Contatti</h1>
                 <div class="row text-center">
-                <div class="col-12 col-sm-6 col-md-4">
-                    <strong>Dove Siamo:</strong><br>
-                    Via Tommaso da Celano 104/106<br>
-                    (Rm) 00179
-                </div>
-                <div class="col-12 col-sm-6 col-md-4">
-                    <strong>Email</strong><br>
-                    <p>ilcerchiomagico1@gmail.com</p>
-                </div>
-                <div class="col-12 col-sm-12 col-md-4">
-                    <strong>Telefono</strong><br>
-                    351613999 (Sede Roma)<br>
-                    3515105824 (Sede Fiumicino)
-                </div>
+                  <div class="col-12 col-sm-6 col-md-4">
+                      <strong>Dove Siamo:</strong><br>
+                      Via Tommaso da Celano 104/106<br>
+                      (Rm) 00179
+                  </div>
+                  <div class="col-12 col-sm-6 col-md-4">
+                      <strong>Email</strong><br>
+                      <p>ilcerchiomagico1@gmail.com</p>
+                  </div>
+                  <div class="col-12 col-sm-12 col-md-4">
+                      <strong>Telefono</strong><br>
+                      3513613999 (Sede Roma)<br>
+                      3515105824 (Sede Fiumicino)
+                  </div>
+                  <div class="col-12 mx-5 d-flex width justify-content-end my-3">
+                    <div class="margin ">
+                      <a href="https://www.facebook.com/profile.php?id=100063476351268&locale=it_IT"><i class="fab fa-facebook"></i></a>
+
+                    </div>
+                    <div>
+                      <a href="https://www.instagram.com/associazioneilcerchiomagico/"><i class="fab fa-instagram"></i></a>
+
+                    </div>
+                  </div>
                 </div>
 
             </div>
@@ -33,6 +43,12 @@ export default {
            
   
 
-<style >
-  /* Stili CSS per il componente, se necessario */
+<style lang="scss" scoped>
+    .width{
+      width: 49%;
+    }
+
+    .margin{
+      margin-right: 10px;
+    }
 </style>
